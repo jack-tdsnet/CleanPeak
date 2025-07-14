01de2e33357ca2570f09cc196799e1d430a6bb9f
Jack Pick <jackp@totaldatasolutions.com.au>
2025-07-14 22:57:42 +1000

    chore(cleanpeak): more cleanup.

--- CleanPeak.AseXmlProcessor/ApplicationConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace CleanPeak.AseXmlProcessor
-{
-    public static class ApplicationConfiguration
-    {
-        public static string ParticipantId = "CLEANPEAK";
-        public static int SchemaVersion = 44;
-    }
-}
